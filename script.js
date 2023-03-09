a2b1b2e4efc01c4b722b1038c785a0bdac04973a
Marcus Karty <mn.karty@gmail.com>
2023-03-09 13:16:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+var btn = document.getElementById("wheatbtn");
 var money = 0;
-var timesClicked = 0;
+var level = 0;
 var production = 10000
 var intervalId;
-var upgrade = -100
+
+var upgrad = 100;
+var upCost = 5;
+
+let enuf = 0;
+
+
 
 function counting() {
+
+
     //  Basically, when the button is pressed we increment money by the production variable.
     if (!intervalId) {
         intervalId = self.setInterval(function() {
@@ -13,10 +22,21 @@ function counting() {
             console.log(money+" wheat gathered");
         }, production); // money increments based on production variable
     }
-    timesClicked++;
-    console.log(timesClicked);
-    console.clear();
+    
+    level++
+    document.getElementById("counter").innerHTML = level;   // level of wheat
+    
+    if (level == 100) {
+        console.log("Wheat: Max Level Reached");}
 
-    let formattedNumber = production.toLocaleString();
+    if (level > 99) {
+        level = 99;}
+
+
+    let formattedNumber = production.toLocaleString(); //formats the money per tick.
     console.log(formattedNumber+': tickspeed');
+
 }
+/*
+For every level of wheat, deduct 100 from the production variable.
+*/
